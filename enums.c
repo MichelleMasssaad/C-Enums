eac68b44257faaab09c2de0505de3666ea081c9a
Michelle Massaad <michelle.massaad@epita.fr>
2025-08-31 14:34:54 +0200

    done

---
+++ enums.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+enum Day{ Sun=1, Mon =2 , Tue=3 , Wed=4 ,   Thu=5 , Fri=6 , Sat=7};
+
+int main()
+{
+    enum Day today = Sun;
+
+    if(today==Sun || today==Sat)
+    {
+        printf("\nIt's the weeknd! Party time!");
+
+    }
+    else
+    {
+        printf("\nI have to work today");
+    }
+    return 0;
+}
+
+
